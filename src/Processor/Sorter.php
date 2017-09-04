fed1e09a6f87bb19655fe1c3ae2303dfe097b519
Yuri <yura888840@gmail.com>
2017-09-04 19:22:39 +0300

    Sorter implementation + readme

--- src/Processor/Sorter.php
+++ src/Processor/Sorter.php
@@ -27,14 +27,52 @@ class Sorter
      */
     public function sort()
     {
+        $found = false;
         $cards = $this->cards;
-        $output = [];
 
-        /** @var AbstractVehicleBoardingCard $current */
-        while (($current = array_shift($cards)) !== null) {
+        $k = 0;
+        while (!$found) {
+            /** @var AbstractVehicleBoardingCard $current */
+            $rootNode = array_shift($cards);
 
+            $cardsProcessing = $this->cards;
+            unset($cardsProcessing[$k]);
+
+            $found = true;
+            /** @var AbstractVehicleBoardingCard $card */
+            foreach ($cardsProcessing as $card) {
+                if ($card->arrival == $rootNode->departure) {
+                    $found = false;
+                    break;
+                }
+            }
+            $k++;
         }
 
+        $sorted     = [];
+        $sorted[]   = $rootNode;
+        $cards      = [];
+
+        foreach ($this->cards as $value) {
+            if ($value != $rootNode) {
+                $cards[] = $value;
+            }
+        }
+
+        while (!empty($cards)) {
+            /** @var AbstractVehicleBoardingCard $value */
+            foreach ($cards as $k => $value) {
+                if ($rootNode->arrival == $value->departure) {
+                    $sorted[] = $value;
+                    unset($cards[$k]);
+
+                    $rootNode = $value;
+                    break;
+                }
+            }
+        }
+
+        $this->cards = $sorted;
     }
 
     public function getSortedCards()
